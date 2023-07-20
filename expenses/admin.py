e90912ecebb4fa1fadd0208e01709ed9504476b7
khaled <khaledgamal1345@gmail.com>
2023-07-20 09:50:58 +0300

    make crud

--- expenses/admin.py
+++ expenses/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Category , Expense
 # Register your models here.
+
+admin.site.register(Expense)
+admin.site.register(Category)
+
+
+
+
+# to create superuser use -> winpty python manage.py createsuperuser
